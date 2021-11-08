bf294b2d4851d8d0ef837da7183eb97c809d7d93
DESKTOP-DDP2VLM\abiao <abiaodev@qq.com>
2021-11-08 11:52:00 +0800

    Create to_html.sh

---
+++ to_html.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+z -f markdown -t html -o resume-eng.html resume-eng.md -T "Mengjie Cai's Resume" -c css/main.css
+pandoc -f markdown -t html -o resume-cn.html resume-cn.md -T "Mengjie Cai's Resume" -c css/main.css
